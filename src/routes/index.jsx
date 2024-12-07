4b349d26d69af430c5a839f8cbbbf10a477363a8
Stanyslaus Hary Muntoro <67821697+staryone@users.noreply.github.com>
2024-12-07 18:46:25 +0700

    fix: memperbaiki import jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,76 +2,75 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 import MainLayout from "../layouts/MainLayout.jsx";
 import HomePage from "../pages/auth/user/HomePage.jsx";
-import LoginUSer from "../pages/user/LoginUser";
-import Profile from "../pages/Profile";
-import Payment from "../pages/Payment";
-import Upcoming from "../pages/Upcoming";
-import ManageBid from "../pages/ManageBid";
-import Information from "../pages/Information";
-import Catalog from "../pages/Catalog";
-import CarDetails from "../pages/CarDetails";
-import AddBid from "../pages/AddBid";
-
+import LoginUser from "../pages/auth/user/LoginUser";
+// import Profile from "../pages/Profile";
+// import Payment from "../pages/Payment";
+// import Upcoming from "../pages/Upcoming";
+// import ManageBid from "../pages/ManageBid";
+// import Information from "../pages/Information";
+// import Catalog from "../pages/Catalog";
+// import CarDetails from "../pages/CarDetails";
+// import AddBid from "../pages/AddBid";
 
 const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <div>Routes Not Found!</div>,
-  },
-  {
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      //   {
-      //     path: "/login",
-      //     element: <LoginUser />,
-      //   },
-      //   {
-      //     path: "/profile",
-      //     element: <Profile />,
-      //   },
-      //   {
-      //     path: "/payment",
-      //     element: <Payment />,
-      //   },
-      //   {
-      //     path: "/upcoming",
-      //     element: <Upcoming />,
-      //   },
-      //   {
-      //     path: "/managebid",
-      //     element: <ManageBid />,
-      //   },
-      //   {
-      //     path: "/information",
-      //     element: <Information />,
-      //   },
-      //   {
-      //     path: "/catalog",
-      //     element: <Catalog />,
-      //   },
-      //   {
-      //     path: "/cardetails",
-      //     element: <CarDetails />,
-      //   },
-      //   {
-      //     path: "/addbid",
-      //     element: <AddBid />,
-      //   },
-    ],
-  },
+    {
+        path: "*",
+        element: <div>Routes Not Found!</div>,
+    },
+    {
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            //   {
+            //     path: "/login",
+            //     element: <LoginUser />,
+            //   },
+            //   {
+            //     path: "/profile",
+            //     element: <Profile />,
+            //   },
+            //   {
+            //     path: "/payment",
+            //     element: <Payment />,
+            //   },
+            //   {
+            //     path: "/upcoming",
+            //     element: <Upcoming />,
+            //   },
+            //   {
+            //     path: "/managebid",
+            //     element: <ManageBid />,
+            //   },
+            //   {
+            //     path: "/information",
+            //     element: <Information />,
+            //   },
+            //   {
+            //     path: "/catalog",
+            //     element: <Catalog />,
+            //   },
+            //   {
+            //     path: "/cardetails",
+            //     element: <CarDetails />,
+            //   },
+            //   {
+            //     path: "/addbid",
+            //     element: <AddBid />,
+            //   },
+        ],
+    },
 ]);
 
 const AppRouter = () => {
-  return (
-    <>
-      <Toaster position="top-center" richColors />
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <Toaster position="top-center" richColors />
+            <RouterProvider router={router} />
+        </>
+    );
 };
 
 export default AppRouter;
