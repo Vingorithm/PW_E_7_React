9a4f4c79d99d0d82230e3cd5178d0cdbfefdaaa1
Stanyslaus Hary Muntoro <67821697+staryone@users.noreply.github.com>
2024-12-18 04:41:05 +0700

    fix: memperbaiki respsive header admin

--- src/layouts/AdminLoginLayout.jsx
+++ src/layouts/AdminLoginLayout.jsx
@@ -2,18 +2,19 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "../components/Header";
+// import Header from "../components/Header";
+import HeaderAdmin from "../components/HeaderAdmin";
 
 const routes = [];
 
 const AdminLoginLayout = () => {
-  return (
-    <div>
-      <Header routes={routes} />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <HeaderAdmin routes={routes} />
+            <Outlet />
+            <Footer />
+        </div>
+    );
 };
 
 export default AdminLoginLayout;
