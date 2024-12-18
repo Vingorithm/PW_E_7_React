37c7249de90ec075c228b7aa0070559426cb6e17
StaryOne <stanyslausharyone@gmail.com>
2024-12-18 10:22:14 +0700

    fix: memperbaiki fitur add bid yang terhubung ke databse

--- src/clients/apiBids.js
+++ src/clients/apiBids.js
@@ -1,14 +1,10 @@
 import { apiClient } from "../api/apiClient";
 import { ENDPOINTS } from "../api/endpoints";
 
-export const CreateBid = (bid_price, auction_id) => {
+export const CreateBid = (data) => {
     // if (!auction_id || !bid_price || !user_id) {
     //     throw new Error("Invalid input: auction_id, bid_price, and user_id are required.");
     // }
 
-    return apiClient.post(ENDPOINTS.CREATE_BID, {
-        auction_id: parseInt(auction_id, 10),
-        bid_price: parseFloat(bid_price),
-        // user_id: user_id,
-    });
+    return apiClient.post(ENDPOINTS.CREATE_BID, data);
 };
